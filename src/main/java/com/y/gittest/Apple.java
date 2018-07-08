420af818217e15ee7938c3c9b18ecd7588633edf
asprial-moon <17317557314@163.com>
2018-07-08 16:04:32 +0800

    branch hot_fix 	first commit

--- src/main/java/com/y/gittest/Apple.java
+++ src/main/java/com/y/gittest/Apple.java
@@ -4,6 +4,7 @@ public class Apple {
 	public void test(){
 		System.out.println("y2 Apple");
 		System.out.println("y Apple");
+		System.out.println("branch hot_fix");
 		int i=0;
 		Object arr=new int[]{10};
 		System.out.println(arr);
