fd867d292ffbae815bcc2348884370cbe4a1388b
asprial-moon <17317557314@163.com>
2018-07-08 15:46:46 +0800

    y2 Git 15:46

--- src/main/java/com/y/gittest/Apple.java
+++ src/main/java/com/y/gittest/Apple.java
@@ -2,7 +2,7 @@ package com.y.gittest;
 
 public class Apple {
 	public void test(){
-		System.out.println("test Apple");
+		System.out.println("y2 Apple");
 		int i=0;
 		Object arr=new int[]{10};
 		System.out.println(arr);
